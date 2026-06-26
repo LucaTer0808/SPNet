d7ab35e91940f7c7f2f45b04969e816caaeb1e26
Luca Nicolas Terfehr <luca.terfehr@outlook.de>
2026-06-26 13:52:27 +0200

    Added time measurement and more scripts

--- test_produce_maps.py
+++ test_produce_maps.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 import torch.nn.functional as F
 import sys
@@ -12,10 +14,12 @@ from Code.utils.data import test_dataset
 parser = argparse.ArgumentParser()
 parser.add_argument('--testsize', type=int, default=352, help='testing size')
 parser.add_argument('--gpu_id',   type=str, default='0', help='select gpu id')
-parser.add_argument('--test_path',type=str, default='./Data/TestDataset/',help='test dataset path')
-opt = parser.parse_args()
+parser.add_argument('--rgb', type=str, default='RGB', help='rgb image path')
+parser.add_argument('--depth', type=str, default='depth', help='depth image path')
+parser.add_argument('--gt', type=str, default='GT', help='gt image path')
+parser.add_argument('--target', type=str, default='target', help='path to save the predicted maps')
 
-dataset_path = opt.test_path
+opt = parser.parse_args()
 
 #set device for test
 if opt.gpu_id=='0':
@@ -30,34 +34,47 @@ model.cuda()
 model.load_state_dict(torch.load('./Checkpoint/SPNet/SPNet_model_best.pth'))
 model.eval()
 
-#test
-test_datasets = ['NJU2K','NLPR', 'DES', 'SSD','SIP', 'STERE'] 
 
-test_datasets = ['STERE'] 
+if not os.path.exists(opt.target):
+    os.makedirs(opt.target)
+        
+image_root  = opt.rgb if opt.rgb.endswith('/') else opt.rgb + '/'
+gt_root     = opt.gt if opt.gt.endswith('/') else opt.gt + '/'
+depth_root  = opt.depth if opt.depth.endswith('/') else opt.depth + '/'
+target_path = opt.target if opt.target.endswith('/') else opt.target + '/'
+eval_path = target_path + 'evaluation.txt'
 
+test_loader = test_dataset(image_root, gt_root,depth_root, opt.testsize)
 
-for dataset in test_datasets:
-    save_path = './test_maps/SPNet/' + dataset + '/'
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+torch.cuda.synchronize()
+start_time = time.time()
+
+for i in range(test_loader.size):
+    print(f'Processing image {i+1}/{test_loader.size}')
+    image, gt,depth, name, image_for_post = test_loader.load_data()
         
-    image_root  = dataset_path + dataset + '/RGB/'
-    gt_root     = dataset_path + dataset + '/GT/'
-    depth_root  = dataset_path + dataset + '/depth/'
-    test_loader = test_dataset(image_root, gt_root,depth_root, opt.testsize)
-    for i in range(test_loader.size):
-        image, gt,depth, name, image_for_post = test_loader.load_data()
+    gt      = np.asarray(gt, np.float32)
+    gt     /= (gt.max() + 1e-8)
+    image   = image.cuda()
+    depth   = depth.cuda()
+    pre_res = model(image,depth)
+    res     = pre_res[2]     
+    res     = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
+    res     = res.sigmoid().data.cpu().numpy().squeeze()
+    res     = (res - res.min()) / (res.max() - res.min() + 1e-8)
         
-        gt      = np.asarray(gt, np.float32)
-        gt     /= (gt.max() + 1e-8)
-        image   = image.cuda()
-        depth   = depth.cuda()
-        pre_res = model(image,depth)
-        res     = pre_res[2]     
-        res     = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
-        res     = res.sigmoid().data.cpu().numpy().squeeze()
-        res     = (res - res.min()) / (res.max() - res.min() + 1e-8)
-        
-        print('save img to: ',save_path+name)
-        cv2.imwrite(save_path+name,res*255)
-    print('Test Done!')
+    cv2.imwrite(target_path + name,res*255)
+
+torch.cuda.synchronize()
+end_time = time.time()
+
+total_time = end_time - start_time
+images_processed = test_loader.size
+average_time_per_image = total_time / images_processed if images_processed > 0 else 0
+
+with open(eval_path, 'w') as f:
+    f.write(f'Total time: {total_time:.4f} seconds\n')
+    f.write(f'Images processed: {images_processed}\n')
+    f.write(f'Average time per image: {average_time_per_image:.4f} seconds\n')
+
+print('Test Done!')
